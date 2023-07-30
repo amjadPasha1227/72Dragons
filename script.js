27941187bb43a146cd270d660e12e3eca0143017
amjadPasha1227 <68808155+amjadPasha1227@users.noreply.github.com>
2023-07-30 22:40:20 +0530

    File uploads

---
+++ script.js
@@ -0,0 +1,6 @@
+function toggleMenu() {
+    let menuIcon = document.getElementById("menu_icon");
+    let Menu = document.getElementById("menu");
+    menuIcon.classList.toggle("open");
+    Menu.classList.toggle("open")
+ }
